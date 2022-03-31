8205f528c4a794a68b0271c3d03cc10207712161
scisneros <santi.cisneros@live.com.ar>
2022-03-30 21:25:12 -0300

    arrlego ruta home

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.listen(PORT);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"))
 });
 
