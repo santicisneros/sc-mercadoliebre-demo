c40e8f307ac5587c32d4c376dc96c507a3cc5337
scisneros <santi.cisneros@live.com.ar>
2022-03-30 21:11:13 -0300

    cambios

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("El servidor de prendio en el puerto:3000");
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"))
